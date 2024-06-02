8af9b306df73db97d497671bc66423c295f97d37
Timucin Besken <timucin@besken.ch>
2024-06-02 18:40:00 +0200

    home(timp4w): add kitty

---
+++ home/common/apps/kitty.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+{
+  programs.kitty = {
+    enable = true;
+
+    settings = {
+      window_padding_width = "10";
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+
+      font_size = "11.0";
+
+      background_opacity = "50";
+    };
+
+    font = {
+      package = (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; });
+      name = "FiraCode";
+    };
+  };
+
+}
--- home/timp4w.nix
+++ home/timp4w.nix
@@ -7,6 +7,7 @@
 
     ./common/apps/brave.nix
     ./common/apps/nvim
+    ./common/apps/kitty.nix
 
     ./common/gaming
 
