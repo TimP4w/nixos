05bb73d30390f1d184aef1bec469f01ac2d83234
Timucin Besken <timucin@besken.ch>
2024-11-30 05:09:35 +0100

    nixos: use bluetooth module, remove rsync, add plymouth, disable hyprland

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -7,31 +7,29 @@
 
   boot = {
     kernelPackages = pkgs.linuxPackages;
-    kernelParams = [ "quiet" ]; # "splash" is breaking stuf...
-    initrd.systemd.enable = false; # Breaks a lot of things (login crash, keyring not unlocking, etc.)
-    plymouth.enable = false;
+    kernelParams = [ 
+      "quiet" 
+      "splash" 
+      "boot.shell_on_fail"  
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    initrd.systemd.enable = true;
+    plymouth = {
+      enable = true;
+      theme = "hexagon_dots_alt";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["hexagon_dots_alt"];
+        })
+      ];
+    };
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    #initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_drm" "nvidia_uvm" ];
   };
-
-  ## Backup Disks RAID
-  #
-  # `$ sudo mdadm --assemble --scan`
-  # `$ sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf`
-  #
-  # fileSystems."/mnt/raid" = {
-  #   device = "/dev/md0";
-  #   fsType = "ext4";
-  #   options = [ "defaults" ];
-  # };
-  # systemd.services.mdadm = {
-  #   description = "MDADM RAID arrays";
-  #   wantedBy = [ "multi-user.target" ];
-  #   after = [ "local-fs.target" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.mdadm}/bin/mdadm --assemble --scan";
-  #     ExecStop = "${pkgs.mdadm}/bin/mdadm --stop --scan";
-  #     RemainAfterExit = true;
-  #   };
-  # };
 
   environment.systemPackages = with pkgs; [
     python3
@@ -66,7 +64,7 @@
       enableRocksmith2014 = true; # Needs reboot if toggled
     };
     gnome.enable = true;
-    hyprland.enable = true;
+    hyprland.enable = false;
     grub.enable = true;
     ld.enable = true;
     network.enable = true;
@@ -74,6 +72,7 @@
     zsh.enable = true;
     password-manager.enable = true;
     logitech.enable = true;
+    bluetooth.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -83,20 +82,33 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
-  hardware.bluetooth.settings = {
-    General = {
-      Experimental = true;
-    };
-  };
+  # TODO Failed to write ATTR{/sys/devices/pci0000:00/0000:00:14.0/usb1/1-3/1-3:1.3/power/control}="on", ignoring: No such file or directory
+  #services.udev.extraRules = ''
+  #  ACTION=="add", SUBSYSTEM=="usb", ATTR{power/control}="on"
+  #'';
 
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="usb", ATTR{power/control}="on"
-  '';
+  ## Backup Disks RAID
+  #
+  # `$ sudo mdadm --assemble --scan`
+  # `$ sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf`
+  #
+  # fileSystems."/mnt/raid" = {
+  #   device = "/dev/md0";
+  #   fsType = "ext4";
+  #   options = [ "defaults" ];
+  # };
+  # systemd.services.mdadm = {
+  #   description = "MDADM RAID arrays";
+  #   wantedBy = [ "multi-user.target" ];
+  #   after = [ "local-fs.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.mdadm}/bin/mdadm --assemble --scan";
+  #     ExecStop = "${pkgs.mdadm}/bin/mdadm --stop --scan";
+  #     RemainAfterExit = true;
+  #   };
+  # };
 
-  /*
+    /*
 
     fileSystems."/mnt/k3s" = {
     device = "192.168.1.3:/volume1/k3s";
@@ -104,6 +116,7 @@
     options = [ "nfsvers=4.1" ];
     };
   */
+
 
   # Hack to avoid a suspending loop after waking up. Mind that this system is NOT a laptop and doesn't have a lid... 
   services.logind.lidSwitchExternalPower = "ignore";
